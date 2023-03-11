a9dee28e121a4a97d3b5c0a80b81c562ceede746
user <user@LAPTOP-TT8LND4V>
2023-03-11 11:38:00 +0530

    First Commit

---
+++ HotelBilling/src/OrderPrice.java
@@ -0,0 +1,111 @@
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JTextArea;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.SwingConstants;
+import java.awt.Color;
+import javax.swing.JTextField;
+import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.io.FileWriter;
+import java.io.PrintWriter;
+import java.awt.event.ActionEvent;
+import java.awt.Font;
+
+public class OrderPrice {
+
+	private JFrame frame;
+	private JTextField txtbxitem;
+	private JTextField txtbxcost;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					OrderPrice window = new OrderPrice();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public OrderPrice() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		frame.setBounds(100, 100, 450, 300);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.getContentPane().setLayout(null);
+		
+		JLabel txtitem = new JLabel("ITEM");
+		txtitem.setFont(new Font("Arial", Font.PLAIN, 15));
+		txtitem.setHorizontalAlignment(SwingConstants.CENTER);
+		txtitem.setBackground(new Color(0, 128, 255));
+		txtitem.setBounds(62, 50, 74, 30);
+		frame.getContentPane().add(txtitem);
+		
+		JLabel txtcost = new JLabel("COST");
+		txtcost.setFont(new Font("Arial", Font.PLAIN, 15));
+		txtcost.setHorizontalAlignment(SwingConstants.CENTER);
+		txtcost.setBounds(62, 114, 74, 30);
+		frame.getContentPane().add(txtcost);
+		
+		txtbxitem = new JTextField();
+		txtbxitem.setFont(new Font("Arial", Font.PLAIN, 15));
+		txtbxitem.setBounds(223, 50, 96, 30);
+		frame.getContentPane().add(txtbxitem);
+		txtbxitem.setColumns(10);
+		
+		txtbxcost = new JTextField();
+		txtbxcost.setFont(new Font("Arial", Font.PLAIN, 15));
+		txtbxcost.setBounds(223, 114, 96, 30);
+		frame.getContentPane().add(txtbxcost);
+		txtbxcost.setColumns(10);
+		
+		JButton btnNewButton = new JButton("SUBMIT");
+		btnNewButton.setFont(new Font("Arial", Font.PLAIN, 15));
+		btnNewButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				
+				String item=txtbxitem.getText();
+				String cost=txtbxcost.getText();
+				
+				FileWriter fw;
+				try {
+					fw= new FileWriter("D:\\Java Class\\Project\\ItemCost.txt",true);
+					
+					PrintWriter p= new PrintWriter(fw);
+					p.println(item.trim()+ "," + cost.trim());
+					p.close();
+					
+					JOptionPane.showMessageDialog(null,"Item Saved Successfully");
+					txtbxitem.setText("");
+					txtbxcost.setText("");
+					
+				}
+				catch(Exception  e1)
+				{
+					
+				}
+				
+			}
+		});
+		btnNewButton.setBounds(278, 203, 107, 30);
+		frame.getContentPane().add(btnNewButton);
+	}
+}
---
+++ HotelBilling/src/TablesOrder.java
@@ -0,0 +1,113 @@
+import java.awt.Container;
+import java.awt.EventQueue;
+import java.awt.event.ActionEvent;
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JCheckBox;
+import javax.swing.JComboBox;
+import java.awt.Font;
+import javax.swing.SwingConstants;
+
+public class TablesOrder {
+
+	private JFrame frame;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					TablesOrder window = new TablesOrder();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 * @throws IOException 
+	 * @throws FileNotFoundException 
+	 */
+	public TablesOrder() throws FileNotFoundException, IOException {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() throws FileNotFoundException, IOException {
+		frame = new JFrame();
+		frame.setBounds(100, 100, 450, 300);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.getContentPane().setLayout(null);
+		
+		JLabel lblNewLabel = new JLabel("New label");
+		lblNewLabel.setHorizontalAlignment(SwingConstants.CENTER);
+		lblNewLabel.setFont(new Font("Arial", Font.PLAIN, 15));
+		lblNewLabel.setBounds(66, 41, 98, 30);
+		frame.getContentPane().add(lblNewLabel);
+		
+		JComboBox comboBox = new JComboBox();
+		comboBox.setFont(new Font("Arial", Font.PLAIN, 12));
+    	comboBox.setBounds(198, 41, 143, 34);
+		frame.getContentPane().add(comboBox);
+		
+				comboBox.addItem("Select a number");
+				
+		        // Read numbers from the text document
+		        try {
+		        	BufferedReader reader = new BufferedReader(new FileReader("D:\\Java Class\\Project\\TablesList.txt"));
+		            String line;
+		            while ((line = reader.readLine()) != null) {
+		            	comboBox.addItem(line.trim());
+		            }
+		            reader.close();
+		        } catch (IOException e1) {
+		            e1.printStackTrace();
+		        }
+  
+		 String eachline;
+		 JPanel panel = new JPanel();
+		 int y=85;
+				try {
+					BufferedReader reader = new BufferedReader(new FileReader("D:\\Java Class\\Project\\ItemCost.txt"));
+					while ((eachline = reader.readLine()) != null) 
+					{
+					    String cols[] = eachline.split(",");
+					    	//System.out.println(cols[0]);
+				    	JCheckBox c = new JCheckBox(cols[0]);
+								c.setBounds(6, y, 97, 23);
+							frame.getContentPane().add(c);	    
+							y+=20;
+					   
+				}
+					reader.close();
+				
+				}
+
+					catch (FileNotFoundException e) {
+					e.printStackTrace();
+					}
+				frame.add(panel);
+		        frame.setVisible(true);
+		        
+		        
+		 
+				
+	}	
+}
+				
+				
+				
+				
